d179c67676be6e032954880e46a98979e3c1d6b9
GetALifeHahaha <sencioahlannour@gmail.com>
2026-06-17 10:44:31 +0800

    Successfully auto-fetched information from OWM through Celery

--- backend/rainfall_fetch/tasks.py
+++ backend/rainfall_fetch/tasks.py
@@ -1,9 +1,15 @@
-from celery import shared_task
 import requests
 import logging
-from backend.settings import OPEN_WEATHER_MAP_API
+
+from celery import shared_task
+
+from django.utils import timezone
+from django.conf import settings
+
+from barangays.models import Barangay
 
 logger = logging.getLogger(__name__)
+OPEN_WEATHER_MAP_API = settings.OPEN_WEATHER_MAP_API
 
 def rainfall_url(latitude: float, longitude: float) -> str:
     return f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={OPEN_WEATHER_MAP_API}"
@@ -11,10 +17,20 @@ def rainfall_url(latitude: float, longitude: float) -> str:
 @shared_task
 def fetch_rainfall_information():
     # fetch data from api
-    response = requests.get(rainfall_url(6.921442, 122.079027))
-    logger.info("Fetching rainfall data.")
-    data = response.json()
-    logger.debug(f"Received rainfall data: {data}")
+    barangays = Barangay.objects.all()
+    readings = []
+
+    for barangay in barangays:
+        centroid = barangay.boundary.centroid
+
+        response = requests.get(
+            rainfall_url(centroid.y, centroid.x),
+            timeout=10 
+        )
+        response.raise_for_status()
+        data = response.json()
+
+        logger.info(f"Fetched rainfall information for {barangay.name}")
     
     # clean data
     # store data using model
