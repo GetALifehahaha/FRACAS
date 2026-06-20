54f9f8ef769d2df01ae35fbadd506b43dd8c5a1f
GetALifeHahaha <sencioahlannour@gmail.com>
2026-06-20 12:32:57 +0800

    Implemented 4 Risk Cards

--- frontend/src/features/gis/Dashboard.tsx
+++ frontend/src/features/gis/Dashboard.tsx
@@ -1,12 +1,48 @@
 import React from 'react'
 import GISMap from './component/GISMap'
+import RiskCard from './component/RiskCard'
+import type { RiskCardType } from './types/RiskType'
 
 const Dashboard = () => {
-  return (
-    <>
-      <GISMap />
-    </>
-  )
+
+	const Risks: RiskCardType[] = [
+		{
+			risk: "critical",
+			barangays: [
+				{name: "Tumaga", link: ""},
+				{name: "Mercedes", link: ""},
+			]
+		},
+		{
+			risk: "high",
+			barangays: null
+		},
+		{
+			risk: "medium",
+			barangays: null
+		},
+		{
+			risk: "low",
+			barangays: null
+		},
+	]
+
+	const riskList = Risks.map(({risk, barangays}, index) => 
+		<RiskCard 
+			key={index}
+			risk={risk}
+			barangays={barangays}
+			/>
+	);
+
+  	return (
+    	<>
+			<div className='absolute top-4 right-4 grid grid-cols-2 gap-2 z-2 w-1/4'>
+				{riskList}
+			</div>
+      		<GISMap />
+    	</>
+  	)
 } 
 
 export default Dashboard
--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -5,7 +5,7 @@ const Layout = () => {
   return (
     <div className='w-full h-screen min-h-screen flex flex-col'>
         <Header />
-        <div className='flex-1 p-2'>
+        <div className='flex-1 p-2 relative'>
           <Outlet />
         </div>
     </div>
