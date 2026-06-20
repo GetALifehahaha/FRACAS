104f61e666162c2ec75bbef70ee9c7444b1ebab1
GetALifeHahaha <sencioahlannour@gmail.com>
2026-06-20 13:48:30 +0800

    Added interactability on RiskCards

--- frontend/src/features/gis/component/RiskCard.tsx
+++ frontend/src/features/gis/component/RiskCard.tsx
@@ -3,7 +3,7 @@ import { Card } from '@/common/ui/card';
 import { cn } from '@/common/utils/utils';
 import type { RiskCardType } from '../types/RiskType';
 import capitalize from '@/common/utils/capitalize';
-
+import { Button } from '@/common/ui/button';
 
 
 const RiskCard = ({
@@ -13,12 +13,13 @@ const RiskCard = ({
 
     const [isExpanded, setExpanded] = useState<boolean>(false);
 
-    const toggleExpanded = (): void => setExpanded(e => !e);
+    const toggleExpanded = (): void => {if (barangays) setExpanded(e => !e)};
 
     const barangaysList = barangays?.map(({name, link}, index) => 
-        <h5 className='' key={index} onClick={(e) => {e.stopPropagation(); console.log(link)}}>
-            {name}
-        </h5>
+        <Card size='sm' className='flex-row items-center justify-between px-2' key={index} onClick={(e) => e.stopPropagation()} >
+            <h5>{name}</h5>
+            <Button variant="outline" className="cursor-pointer" onClick={() => console.log(link)}>View</Button>
+        </Card>
     )
 
     return (
@@ -30,10 +31,11 @@ const RiskCard = ({
                 risk === "medium" && 'bg-yellow-500',
                 risk === "high" && 'bg-orange-500',
             )} />
-            <div className='flex flex-col gap-1 items-start'>
+            <div className='flex flex-1 flex-col gap-1 items-start'>
                 <h5 className='font-semibold text-md'>{capitalize(risk)}</h5>
                 {isExpanded ?
-                    <div>
+                    <div className='flex flex-col gap-1 w-full mt-4'>
+                        <h5 className='font-semibold'>Affected Barangays:</h5>
                         {barangaysList}
                     </div>
                     :
